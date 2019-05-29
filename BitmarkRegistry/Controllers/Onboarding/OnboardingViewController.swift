cc358fbb5ebdfa4e137e7fb30b62e764c3d9b68c
Thuyen Truong <thuyen@bitmark.com>
2019-05-29 11:58:48 +0700

    [f] Create Onboarding Layout

---
+++ BitmarkRegistry/Controllers/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,13 @@
+//
+//  OnboardingViewController.swift
+//  BitmarkRegistry
+//
+//  Created by Macintosh on 5/29/19.
+//  Copyright © 2019 thuyentruong. All rights reserved.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+  
+}
