0862fa53f2fafc2485b31a0458e357c70026d130
Thuyen Truong <thuyen@bitmark.com>
2019-05-31 09:28:05 +0700

    [f] Create Properties Tab + Yours Tab with Empty

---
+++ BitmarkRegistry/Helpers/PropertySegment.swift
@@ -0,0 +1,15 @@
+//
+//  PropertySegment.swift
+//  BitmarkRegistry
+//
+//  Created by Macintosh on 5/31/19.
+//  Copyright © 2019 thuyentruong. All rights reserved.
+//
+
+import Foundation
+
+enum PropertySegment: Int {
+  case Yours
+  case Tracked
+  case Global
+}
