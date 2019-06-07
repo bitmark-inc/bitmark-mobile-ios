1112ea5e030a1e9d2bffc4249bbdfa8e3957c3f5
Thuyen Truong <thuyen@bitmark.com>
2019-06-07 10:49:45 +0700

    [c] Add Secret Test Key into Info.plist instead of separate keys.plist

--- BitmarkRegistry/Helpers/Credential.swift
+++ BitmarkRegistry/Helpers/Credential.swift
@@ -11,7 +11,7 @@ import Foundation
 class Credential {
 
   public static func valueForKey(keyName:String) -> String {
-    if let filePath = Bundle.main.path(forResource: "keys", ofType: "plist"),
+    if let filePath = Bundle.main.path(forResource: "Info", ofType: "plist"),
       let plist = NSDictionary(contentsOfFile: filePath) {
       return plist.value(forKey: keyName) as! String
     }
