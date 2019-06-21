f59f145d154b139b62595187441fbbc82f7a31c0
Thuyen Truong <thuyen@bitmark.com>
2019-06-21 08:59:57 +0700

    [f] create direct transfer bitmark to other account

---
+++ BitmarkRegistry/Controllers/Main/Properties/TransferBitmarkViewController.swift
@@ -0,0 +1,160 @@
+//
+//  TransferBitmarkViewController.swift
+//  BitmarkRegistry
+//
+//  Created by Macintosh on 6/17/19.
+//  Copyright © 2019 thuyentruong. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+class TransferBitmarkViewController: UIViewController, UITextFieldDelegate {
+
+  // MARK: - Properties
+  var assetName: String!
+  var bitmarkId: String!
+
+  var recipientAccountNumberTextfield: DesignedTextField!
+  var errorForInvalidAccountNumber: UILabel!
+  var transferButton: SubmitButton!
+  var transferButtonBottomConstraint: Constraint!
+
+  // MARK: - Init
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    title = assetName
+    setupViews()
+    setupEvents()
+  }
+
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    addNotificationObserver(name: UIWindow.keyboardWillShowNotification, selector: #selector(keyboardWillBeShow))
+    addNotificationObserver(name: UIWindow.keyboardWillHideNotification, selector: #selector(keyboardWillBeHide))
+  }
+
+  override func viewWillDisappear(_ animated: Bool) {
+    removeNotificationsObserver()
+  }
+
+  // MARK: - Handlers
+  @objc func changeRecipientTextField(textfield: UITextField) {
+    transferButton.isEnabled = !textfield.text!.isEmpty
+  }
+
+  @objc func beginEditing(textfield: UITextField) {
+    errorForInvalidAccountNumber.isHidden = true
+  }
+
+  @objc func tapToTransfer(button: UIButton) {
+    view.endEditing(true)
+    let recipientAccountNumber = recipientAccountNumberTextfield.text!
+    if recipientAccountNumber.isValid() {
+      do {
+        _ = try BitmarkService.directTransfer(
+          account: Global.currentAccount!,
+          bitmarkId: bitmarkId,
+          to: recipientAccountNumber
+        )
+      } catch let e {
+        showErrorAlert(message: e.localizedDescription)
+      }
+    } else {
+      errorForInvalidAccountNumber.isHidden = false
+    }
+  }
+
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    view.endEditing(true)
+  }
+
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    return view.endEditing(true)
+  }
+}
+
+// MARK: - Setup Views/Events
+extension TransferBitmarkViewController {
+  fileprivate func setupEvents() {
+    recipientAccountNumberTextfield.delegate = self
+    recipientAccountNumberTextfield.addTarget(self, action: #selector(changeRecipientTextField), for: .editingChanged)
+    recipientAccountNumberTextfield.addTarget(self, action: #selector(beginEditing), for: .editingDidBegin)
+
+    transferButton.addTarget(self, action: #selector(tapToTransfer), for: .touchUpInside)
+  }
+
+  fileprivate func setupViews() {
+    view.backgroundColor = .white
+
+    // *** Setup subviews ***
+    let transferTitle = CommonUI.inputFieldTitleLabel(text: "TRANSFER BITMARK")
+
+    recipientAccountNumberTextfield = DesignedTextField(placeholder: "RECIPIENT BITMARK ACCOUNT NUMBER")
+    recipientAccountNumberTextfield.clearButtonMode = .always
+    recipientAccountNumberTextfield.returnKeyType = .done
+
+    errorForInvalidAccountNumber = CommonUI.errorFieldLabel(text: "Invalid bitmark account number!")
+    errorForInvalidAccountNumber.isHidden = true
+
+    let descriptionLabel = CommonUI.descriptionLabel(text: "Enter the Bitmark account number to which you would like to transfer ownership of this property.")
+
+    let mainView = UIView()
+    mainView.addSubview(transferTitle)
+    mainView.addSubview(recipientAccountNumberTextfield)
+    mainView.addSubview(errorForInvalidAccountNumber)
+    mainView.addSubview(descriptionLabel)
+
+    transferTitle.snp.makeConstraints { (make) in
+      make.top.leading.trailing.equalToSuperview()
+    }
+
+    recipientAccountNumberTextfield.snp.makeConstraints { (make) in
+      make.top.equalTo(transferTitle.snp.bottom).offset(15)
+      make.leading.trailing.equalToSuperview()
+    }
+
+    errorForInvalidAccountNumber.snp.makeConstraints { (make) in
+      make.top.equalTo(recipientAccountNumberTextfield.snp.bottom).offset(9)
+      make.leading.trailing.equalToSuperview()
+    }
+
+    descriptionLabel.snp.makeConstraints { (make) in
+      make.top.equalTo(errorForInvalidAccountNumber.snp.bottom).offset(15)
+      make.leading.trailing.equalToSuperview()
+    }
+
+    transferButton = SubmitButton(title: "TRANSFER")
+
+    // *** Setup UI in view ***
+    view.addSubview(mainView)
+    view.addSubview(transferButton)
+
+    mainView.snp.makeConstraints { (make) in
+      make.edges.equalTo(view.safeAreaLayoutGuide)
+          .inset(UIEdgeInsets(top: 25, left: 20, bottom: 25, right: 20))
+    }
+
+    transferButton.snp.makeConstraints { (make) in
+      make.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+      transferButtonBottomConstraint = make.bottom.equalTo(view.safeAreaLayoutGuide).constraint
+    }
+  }
+}
+
+// MARK: - KeyboardObserver
+extension TransferBitmarkViewController {
+  @objc func keyboardWillBeShow(notification: Notification) {
+    guard let userInfo = notification.userInfo else { return }
+    let keyboardSize = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as! CGRect
+
+    transferButtonBottomConstraint.update(offset: -keyboardSize.height + view.safeAreaInsets.bottom)
+    view.layoutIfNeeded()
+  }
+
+  @objc func keyboardWillBeHide(notification: Notification) {
+    transferButtonBottomConstraint.update(offset: 0)
+    view.layoutIfNeeded()
+  }
+}
