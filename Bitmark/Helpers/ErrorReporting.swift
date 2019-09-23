619be19a0b88f866be8f3bae0e2d86a603cfef0f
Anh Nguyen <anhnguyen@bitmark.com>
2019-09-23 14:04:11 +0700

    Remove redundant methods

--- Bitmark/Helpers/ErrorReporting.swift
+++ Bitmark/Helpers/ErrorReporting.swift
@@ -33,31 +33,6 @@ struct ErrorReporting {
       Client.shared?.dist = appVersion
     }
   }
-
-  // Report error to sentry
-  public static func report(error err: Error) {
-    let errorEvent = Event(level: .error)
-    let errMessage = err is String ? (err as? String) : err.localizedDescription
-    errorEvent.message = errMessage ?? ""
-    Client.shared?.appendStacktrace(to: errorEvent)
-    Client.shared?.send(event: errorEvent, completion: nil)
-  }
-
-  public static func report(message: String) {
-    let errorEvent = Event(level: .error)
-    errorEvent.message = message
-    Client.shared?.appendStacktrace(to: errorEvent)
-    Client.shared?.send(event: errorEvent, completion: nil)
-  }
-
-  // Log info to sentry
-  public static func breadcrumbs(info msg: String, category: ReportCategory, traceLog: Bool = true) {
-    let breadcrumb = Breadcrumb(level: .info, category: category.rawValue)
-    breadcrumb.message = msg
-    Client.shared?.breadcrumbs.add(breadcrumb)
-
-    if traceLog { Global.log.info(msg) }
-  }
 }
 
 enum ReportCategory: String {
